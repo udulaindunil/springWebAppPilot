b360464d94faeec559a4924b00ee1afd5d1a5a28
udulaindunil <udulaindunil@gmail.com>
2020-04-01 00:22:43 +0530

    My first commit Created spring boot application and configure properties

---
+++ onlineBookshop/src/main/java/com/angularSpring/onlineBookshop/OnlineBookshopApplication.java
@@ -0,0 +1,13 @@
+package com.angularSpring.onlineBookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookshopApplication.class, args);
+	}
+
+}
---
+++ onlineBookshop/src/test/java/com/angularSpring/onlineBookshop/OnlineBookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularSpring.onlineBookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
