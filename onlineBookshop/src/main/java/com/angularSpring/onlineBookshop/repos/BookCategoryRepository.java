f702ee4165307117a058e9cce97f942e0e3a7679
udulaindunil <udulaindunil@gmail.com>
2020-04-04 01:51:00 +0530

    Linkig added

--- onlineBookshop/src/main/java/com/angularSpring/onlineBookshop/repos/BookCategoryRepository.java
+++ onlineBookshop/src/main/java/com/angularSpring/onlineBookshop/repos/BookCategoryRepository.java
@@ -1,9 +1,12 @@
 package com.angularSpring.onlineBookshop.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.angularSpring.onlineBookshop.entities.BookCategory;
 
+
+@RepositoryRestResource(collectionResourceRel = "bookCategories", path = "bookCategories")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 
 }
